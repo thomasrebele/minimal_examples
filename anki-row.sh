c8b12b8c3af9663df3f719d49a33c1b2a81bd73e
Thomas Rebele <thomas.rebele@googlemail.com>
2017-11-03 19:05:06 +0100

    adding error check to anki-row.sh

--- anki-row.sh
+++ anki-row.sh
@@ -19,4 +19,11 @@ if [ "$no_hierarchy" == "1" ]; then
 	img=${img//\//-}
 fi
 
-echo "$img"$'\t'"$desc"$'\t'"$code"
+desc_lines=$(echo "$desc" | wc -l)
+if [ "$desc_lines" != "1" ]; then
+	echo "one line of description needed, got $desc_lines in $1" 1>&2
+elif [ "$code" == "" ]; then
+	echo "start/end tag missing in $1" 1>&2
+else
+	echo "$img"$'\t'"$desc"$'\t'"$code"
+fi
