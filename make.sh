bfc14091d889a7d56edc9c6364ec94260fa88cca
Thomas Rebele <thomas.rebele@googlemail.com>
2017-11-05 20:46:52 +0100

    adding first example

---
+++ make.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# output directory
+imgout="output/"
+
+mkdir -p $imgout
+
+source common.sh
+
+
+for i in $files; do
+	dir=$(dirname $i)
+	file=$(basename $i)
+	mkdir -p $imgout/$dir
+	png=$imgout/${i%.html}.png
+
+	if [ ! -e $png -o $i -nt $png ]; then
+		full=$(full_path $i)
+		echo "full path: $full"
+		echo "png path: $png"
+		cutycapt --url=file:$full --out=$png
+		convert $png -trim +repage $png
+		if type pngquant > /dev/null; then
+			pngquant --ext .png --force $png
+		fi
+	fi
+
+	if [ "$no_hierarchy" == "1" ]; then
+		nohDir=${imgout/img/img-nh}
+		noh=${i/html/png}
+		noh=$nohDir/web-${noh//\//-}
+		mkdir -p $nohDir
+		if [ ! -e $noh ]; then
+			ln $png $noh
+		fi
+	fi
+
+done
