037660b80a48210b4b70ed2b4c5dcba715fc4060
Thomas Rebele <thomas.rebele@googlemail.com>
2017-11-08 21:42:30 +0100

    parsing git log

--- vis-git.py
+++ vis-git.py
@@ -4,11 +4,32 @@ import subprocess
 
 def run(cmd):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out = ""
+    out = []
     for line in p.stdout.readlines():
-        out += line.decode("utf-8")
+        out += [line.decode("utf-8")]
     return out
 
+class Commit:
+    pass
+
+idToCommit = {}
+commits = []
+
+for line in run("git log --format=format:%H'\t'%P'\t'%s'\t'%an"):
+    c = Commit()
+    c.commit, c.parents, c.message, c.author = line.split('\t')
+    if c.parents == '':
+        c.parents = []
+    else:
+        c.parents = c.parents.split(' ')
+    idToCommit[c.commit] = c
+    commits += [c]
+
+for c in commits:
+    parents = []
+    print(c.parents)
+    for p in c.parents:
+        parents += [idToCommit[p]]
+    c.parents = parents
 
 
-print(run("git log --format=format:%H$'\t'%P$'\t'%s$'\t'%an"))
