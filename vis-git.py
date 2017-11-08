83f0c2393a0a8177640b69ce3e99a390f10b0b35
Thomas Rebele <thomas.rebele@googlemail.com>
2017-11-08 23:02:38 +0100

    very basic visualization

--- vis-git.py
+++ vis-git.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import subprocess
+from jinja2 import Environment
+
+import os
+script_dir=os.path.dirname(os.path.realpath(__file__))
 
 def run(cmd):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -12,24 +16,60 @@ def run(cmd):
 class Commit:
     pass
 
-idToCommit = {}
+id_to_commit = {}
 commits = []
 
+# parse git log
 for line in run("git log --format=format:%H'\t'%P'\t'%s'\t'%an"):
     c = Commit()
-    c.commit, c.parents, c.message, c.author = line.split('\t')
-    if c.parents == '':
-        c.parents = []
+    c.commit, c.parent_ids, c.message, c.author = line.split('\t')
+    if c.parent_ids == '':
+        c.parent_ids = []
     else:
-        c.parents = c.parents.split(' ')
-    idToCommit[c.commit] = c
+        c.parent_ids = c.parent_ids.split(' ')
+    c.children = []
+    id_to_commit[c.commit] = c
     commits += [c]
 
+# replace c.parents by objects
 for c in commits:
     parents = []
-    print(c.parents)
-    for p in c.parents:
-        parents += [idToCommit[p]]
+    for p in c.parent_ids:
+        parent_commit = id_to_commit[p]
+        parent_commit.children += [c]
+        parents += [parent_commit]
     c.parents = parents
 
+used_cols = []
+
+for c in commits:
+    if hasattr(c, "col"): continue
+    c.firstChild = True
+
+    if len(c.children) > 0:
+        child = c.children[0]
+        if child.firstChild:
+            c.col = child.col
+            child.firstChild = False
+            continue
+
+    new_col = 0 if len(used_cols) == 0 else max(used_cols) + 1
+    c.col = new_col
+    used_cols += [new_col]
+
+tikz = ""
+
+i = 0
+for c in commits:
+    c.name = chr(64+len(commits)-i)
+    tikz += "\\node[commit] (" + c.name + ") at (" + str(c.col*0.5) + "," + str(i) + ") {" + c.name + "};\n"
+    i+=1
+
+for c in commits:
+    for p in c.parents:
+        tikz += "\\draw (" + c.name + ") to (" + p.name + ");\n"
+
+with open(script_dir + "/template.tex") as f:
+    template = f.read()
+print(Environment().from_string(template).render(tikz=tikz))
 
