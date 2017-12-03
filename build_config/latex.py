044c1bc648bdc747943d22a1f41adc4c8baa60cc
Thomas Rebele <thomas.rebele@googlemail.com>
2017-12-03 20:21:54 +0100

    fixing image path

--- build_config/latex.py
+++ build_config/latex.py
@@ -5,7 +5,7 @@ def make_goal(path, annotations, fields, **keywords):
     img_path = path.replace("examples/", "").replace(".tex", ".png")
     if keywords.get("no_hierarchy", False):
         img_path = img_path.replace("/", "-")
-    img="<img src=\"latex/" + str(img_path) + "\">"
+    img="<img src=\"" + str(img_path) + "\">"
 
     return img
 
